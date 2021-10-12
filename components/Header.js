c2d7d2d3ea17001304197475d9fc6c18bd3103e9
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2021-10-12 15:13:37 -0700

    built signing page, added redirect url on sign in

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 import {
   SearchIcon,
@@ -13,6 +14,10 @@ import {
 import { HomeIcon } from "@heroicons/react/outline";
 
 function Header() {
+  const { data: session } = useSession();
+
+  console.log(session);
+
   return (
     <div className="shadow-sm border-b bg-white sticky-top z-50">
       <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
@@ -52,22 +57,28 @@ function Header() {
         <div className="flex items-center justify-end space-x-4">
           <HomeIcon className="navButton" />
           <MenuIcon className="h-6 md:hidden cursor-pointer" />
+          {session ? (
+            <>
+              <div className="relative naveButton">
+                <PaperAirplaneIcon className="navButton rotate-45" />
+                <div className="absolute -top-2 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">
+                  3
+                </div>
+              </div>
+              <PlusCircleIcon className="navButton" />
+              <UserGroupIcon className="navButton" />
+              <HeartIcon className="navButton" />
 
-          <div className="relative naveButton">
-            <PaperAirplaneIcon className="navButton rotate-45" />
-            <div className="absolute -top-2 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">
-              3
-            </div>
-          </div>
-          <PlusCircleIcon className="navButton" />
-          <UserGroupIcon className="navButton" />
-          <HeartIcon className="navButton" />
-
-          <img
-            src="https://links.papareact.com/3ke"
-            alt="profile pic"
-            className="h-10 rounded-full cursor-pointer"
-          />
+              <img
+                onClick={signOut}
+                src={session.user.image}
+                alt="profile pic"
+                className="h-10 rounded-full cursor-pointer"
+              />
+            </>
+          ) : (
+            <button onClick={signIn}>Sign In</button>
+          )}
         </div>
       </div>
     </div>
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,13 +1,44 @@
 import React from "react";
+import { getProviders, signIn as SignIntoProvider } from "next-auth/react";
+import Header from "../../components/Header";
 
-function signIn() {
+//browser
+function signIn({ providers }) {
   return (
-    <div>
-      <h3>I am the sign in page</h3>
-    </div>
+    <>
+      <Header />
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-56 px-14 text-center">
+        <img className="w-80" src="https://links.papareact.com/ocw" alt="" />
+        <p className="font-xs italic">
+          This is not a REAL app, it is built for educational purposes only
+        </p>
+        <div className="mt-40">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                className="p-3 rounded-lg bg-blue-500 text-white"
+                onClick={() =>
+                  SignIntoProvider(provider.id, { callbackUrl: "/" })
+                }>
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
-export async function getServerSideProps(context) {}
+// server side render
+export async function getServerSideProps(context) {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+}
 
 export default signIn;
